4c175d6520ef1e7a04cd355fa5995b174d5b6b51
Stephanie Hallberg <stephaniiehallberg@gmail.com>
2023-08-16 14:51:28 +0200

    feat(authntication): styling devise forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    def devise_form_field(form, field_type, field_name, label_text, opts = {})
+      content_tag :div, class: 'field' do
+        concat form.label field_name, label_text
+        if field_type == :check_box
+          concat form.send(field_type, field_name, opts)
+        else
+          concat form.send(field_type, field_name, { class: 'form-control' }.merge(opts))
+        end
+      end
+    end
+  
+  def devise_form_submit(form, text, opts = {})
+    content_tag :div, class: 'actions' do
+      form.submit text, { class: 'btn btn-primary mt-3' }.merge(opts)
+    end
+  end
 end
